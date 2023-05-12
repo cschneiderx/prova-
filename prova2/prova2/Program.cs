aef4034ed18e13c528ff1172c80c42cf03e14791
cschneiderx <126523407+cschneiderx@users.noreply.github.com>
2023-05-12 16:22:54 -0300

    Add files via upload

---
+++ prova2/prova2/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string palavra = Console.ReadLine();
+        char[] caracteres = palavra.ToCharArray();
+        Array.Reverse(caracteres);
+        string palavrainvertida = new string(caracteres);
+
+
+        if (palavrainvertida == palavra)
+        {
+            Console.WriteLine("A palavra é um palindromo");
+        }
+        else
+        {
+            Console.WriteLine("A palavra não é  um palindromo");
+        }
+    }
+}
